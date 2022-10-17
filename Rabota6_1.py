fc5c4f6c50be5cc0cb5081a1978fa91a72e566a6
Anastasia <anastasiaapv@gmail.com>
2022-10-17 17:32:29 +0300

    Сохранение 001

---
+++ Rabota6_1.py
@@ -0,0 +1,15 @@
+# Задача 3. Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+
+# import math
+# num = int(input('Введите число: '))
+
+# listN =[]
+# for i in range(1,num+1):
+#     listN.append((1+1/i)**i)
+    
+# print(listN)
+# print(sum(listN))
+
+num = int(input('Введите число: '))
+k = list(range(1,num+1))
+print (list(map(lambda i: (1+1/i)**i,k)))
---
+++ Rabota6_2.py
@@ -0,0 +1,13 @@
+#   Задача 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+# txt = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {txt}")
+# find_txt = "абв"
+# lst = [i for i in txt.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+txt = input("Введите текст через пробел:\n")
+txt = list(filter(lambda x: 'абв' not in x, txt.split()))
+
+print(f'Результат: {" ".join(txt)}')
+
